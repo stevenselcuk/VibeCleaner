ac03d1dea21eccc1eaf0208aa6656b711075b512
Steven J. Selcuk <stevenjselcuk@gmail.com>
2025-09-12 14:43:41 +0300

    Initial Commit

---
+++ VibeCleaner/VibeCleanerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VibeCleanerApp.swift
+//  VibeCleaner
+//
+//  Created by Steven J. Selcuk on 12.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct VibeCleanerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
